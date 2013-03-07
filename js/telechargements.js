a27c11054a42d5e0e29b75b18a1b1ecf8b91ee33
wabbox <afipperrin@gamil.com>
2013-03-07 20:01:51 +0100

    Page téléchargements
    Changement background sur visible dans telechargement.js

--- js/telechargements.js
+++ js/telechargements.js
@@ -8,20 +8,40 @@ $(document).ready(function(){
 	$('#windows').slideDown();
 	$('#mac').hide();
 	$('#dddd').hide();
+function bgVisible(){
+	if ($('#windows').is(':visible')) {
+		$('.link_pc').css('background-image', 'url(images/picto-telechargements/pc_focus.png)');
+	} else {
+		$('.link_pc').css('background-image', 'url(images/picto-telechargements/pc_blur.png)');
+	}
+	if ($('#mac').is(':visible')) {
+		$('.link_mac').css('background-image', 'url(images/picto-telechargements/mac_focus.png)');		
+	} else {
+		$('.link_mac').css('background-image', 'url(images/picto-telechargements/mac_blur.png)');
+	}
+	if ($('#dddd').is(':visible')) {
+		$('.link_dddd').css('background-image', 'url(images/picto-telechargements/4d_focus.png)');		
+	} else {
+		$('.link_dddd').css('background-image', 'url(images/picto-telechargements/4d_blur.png)');
+	}
+}
+
+	bgVisible();
 	
 	$('#telechargement > header > a').click(function(){
-		if($(this).attr('id')=="link_pc"){
+		if($(this).attr('class')=="link_pc"){
 			$('#windows').slideDown();
 			$('#mac').hide();
 			$('#dddd').hide();
-		} else if($(this).attr('id')=="link_mac"){
+		} else if($(this).attr('class')=="link_mac"){
 			$('#windows').hide();
 			$('#mac').slideDown();
 			$('#dddd').hide();
-		} else if($(this).attr('id')=="link_dddd"){
+		} else if($(this).attr('class')=="link_dddd"){
 			$('#windows').hide();
 			$('#mac').hide();
 			$('#dddd').slideDown();
 		}
+		bgVisible();
 	}); 
 });
