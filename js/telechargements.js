de4111e4bf2e09f7213e361868f9359a99f70913
wabbox <afipperrin@gamil.com>
2013-03-11 12:38:17 +0100

    Page téléchargement ajout css
    JS slide

--- js/telechargements.js
+++ js/telechargements.js
@@ -1,41 +1,22 @@
-$(document).ready(function(){
-	$('#windows').slideDown();
-	$('#mac').hide();
-	$('#dddd').hide();
+$(document).ready(function(){	
 function bgVisible(){
-	if ($('#windows').is(':visible')) {
-		$('.link_pc').css('background-image', 'url(images/picto-telechargements/pc_focus.png)');
-	} else {
-		$('.link_pc').css('background-image', 'url(images/picto-telechargements/pc_blur.png)');
-	}
-	if ($('#mac').is(':visible')) {
-		$('.link_mac').css('background-image', 'url(images/picto-telechargements/mac_focus.png)');		
-	} else {
-		$('.link_mac').css('background-image', 'url(images/picto-telechargements/mac_blur.png)');
-	}
-	if ($('#dddd').is(':visible')) {
-		$('.link_dddd').css('background-image', 'url(images/picto-telechargements/4d_focus.png)');		
-	} else {
-		$('.link_dddd').css('background-image', 'url(images/picto-telechargements/4d_blur.png)');
-	}
+	$('.link_windows').removeClass('link_windows_focus');
+	$('.link_mac').removeClass('link_mac_focus');
+	$('.link_dddd').removeClass('link_dddd_focus');
 }
 
-	bgVisible();
-	
+	$('#mac').hide();
+	$('#dddd').hide();
+	$('.link_windows').addClass('link_windows_focus');
+
 	$('#telechargement > header > a').click(function(){
-		if($(this).attr('class')=="link_pc"){
-			$('#windows').slideDown();
-			$('#mac').hide();
-			$('#dddd').hide();
-		} else if($(this).attr('class')=="link_mac"){
-			$('#windows').hide();
-			$('#mac').slideDown();
-			$('#dddd').hide();
-		} else if($(this).attr('class')=="link_dddd"){
-			$('#windows').hide();
-			$('#mac').hide();
-			$('#dddd').slideDown();
+		var_id = $(this).attr("href")
+		if($(var_id).css('display') != 'block'){
+			bgVisible();
+			$(this).addClass('link_'+var_id.substr(1,8)+'_focus');
+			$("#telechargement > section:visible").slideUp("",function(){
+				$(var_id).slideDown();
+			});
 		}
-		bgVisible();
 	}); 
 });
