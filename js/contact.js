90dcafe195e995374593f6f9776244eece90bdc5
wabbox <afipperrin@gamil.com>
2013-03-15 20:00:01 +0100

    correction bug js form contact

--- js/contact.js
+++ js/contact.js
@@ -1,55 +1,95 @@
 $(document).ready(function() {
-	function  verifInput(idInput){	
-		var regex;
-		if (idInput == 'telephone'){
-			regex = /^(0[1-68-9-7])(?:[ _.-]?(\d{2})){4}$/;
-		}
-		else if (idInput == 'email'){
-			regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-		}
-		else if (idInput == 'nom' || idInput == 'prenom'){
-			regex = /^[a-zA-Z0-9_âäàéèùêëîïô öçñ-]{2,24}$/;
-		}
-		else if (idInput == 'codePostal'){
-			regex = /^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B))[0-9]{3}$/
-		}
-		else{
-			regex = /^.{2,1500}$/;
-		}
-		$('#'+idInput).removeClass();
-		var verif;
-		if(regex.test($('#'+idInput).val())) {
-			$('#'+idInput).addClass('correct');
-			$('#'+idInput+' + .tooltip').hide();
-			return true;
-		} else {
-			$('#'+idInput).addClass('incorrect');
-			$('#'+idInput+' + .tooltip').show();
-			return false;
-		}
+	var data={"input_test":[
+	{
+		"Name":"nom",
+		"id":"#nom",
+		"required": true,
+		"regex": "nom"
+	},
+	{
+		"Name":"prenom",
+		"id":"#prenom",
+		"required": true,
+		"regex": "nom"
+	},
+	{
+		"Name":"email",
+		"id":"#email",
+		"required": true,
+		"regex": "email"
+	},
+	{
+		"Name":"message",
+		"id":"#message",
+		"required": true,
+		"regex": "message"
+	},
+	{
+		"Name":"codePostal",
+		"id":"#codePostal",
+		"regex": "cp"
+	},
+	{
+		"Name":"telephone",
+		"id":"#telephone",
+		"regex": "tel"
 	}
+	]}
+	function styleErreur (id){
+		
+	};
 	$('.tooltip').hide();
 	$("form").submit(function(){
 		$("span").remove(".alert-box");
 		$("span").remove(".success-box");
-		var temp;
 
-		$(":input").each(function(){
-			if ($(this).attr('class') == "obligatoire") {
-				console.log($(this).attr('id'));
+		valid = true;
+
+		for (var i in data.input_test) {
+			id = data.input_test[i].id;
+			regex = data.input_test[i].regex;
+			if (regex == 'tel'){
+				regex = /^(0[1-68-9-7])(?:[ _.-]?(\d{2})){4}$/;
+			}
+			else if (regex == 'email'){
+				regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+			}
+			else if (regex == 'nom'){
+				regex = /^[a-zA-Z0-9_âäàéèùêëîïô öçñ-]{2,24}$/;
+			}
+			else if (regex == 'cp'){
+				regex = /^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B))[0-9]{3}$/
+			}
+			else{
+				regex = /^.{2,1500}$/;
 			};
-			if(verifInput($(this).attr('id'))){
-				
-				temp = true;
+
+			if (data.input_test[i].required){
+				// pour différencier les champs vides et les champs mal rempli séparer la condition
+				if ($(id).val() == "" && !regex.test($(id).val())){
+					$(id).next('.tooltip').fadeIn();
+					$(id).addClass('incorrect');
+					valid = false;
+				} else {
+					$(id).removeClass();
+					$(id).next('.tooltip').fadeOut();
+					$(id).addClass('correct');
+				}
+			} else {
+				if ($(id).val() != "" && !regex.test($(id).val())){
+					$(id).next('.tooltip').fadeIn();
+					$(id).addClass('incorrect');
+					valid = false;
+				} else if ($(id).val() == "") {
+					$(id).removeClass();
+				} else {
+					$(id).removeClass();
+					$(id).next('.tooltip').fadeOut();
+					$(id).addClass('correct');
+				}
 			}
-			else
-			{
-				temp = false;
-				return false;
-			}
-		});
-		
-		if (temp) {
+		};		
+		if (valid) {
 			$.ajax({
 				url: $(this).attr('action'),
 				type: $(this).attr('method'),
